0d441a467a2339751edae27072d23e2fca2e94e1
joseluismunozzuta <joseluismunozzuta@gmail.com>
2021-07-06 21:46:52 -0500

    AJUSTS FINALES

--- src/main/java/com/example/proyecto2/controller/MainController.java
+++ src/main/java/com/example/proyecto2/controller/MainController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
@@ -141,18 +142,26 @@ public class MainController {
         model.addAttribute("datosTabla",tablaInfo);
      return "tabla";
     }
+
     @GetMapping("/editar")
-    public String editar(HttpSession session,Model model, @RequestParam("id") int id, @ModelAttribute("oximetro")Oximetro oximetro){
-        Usuario usuariologueado = (Usuario) session.getAttribute("usuarioLogueado");
-        Oximetro oxi = oximetroRepository.buscarxIdAndNombre(id,usuariologueado.getNombre());
-        if(oxi.getIdoximetro()!=null){
-            oximetro=oxi;
-            model.addAttribute("oximetro",oximetro);
-            System.out.println(oximetro.getActivo());
-            return "editar";
-        }
-        else{
-            model.addAttribute("mensaje","No hay pulsioxímetro asociado");
+    public String editar(HttpSession session, Model model, @RequestParam("id") String idstring, @ModelAttribute("oximetro")Oximetro oximetro, RedirectAttributes attributes){
+        try{
+            int id = Integer.parseInt(idstring);
+            Usuario usuariologueado = (Usuario) session.getAttribute("usuarioLogueado");
+            Oximetro oxi = oximetroRepository.buscarxIdAndNombre(id,usuariologueado.getNombre());
+            if(oxi != null){
+                oximetro=oxi;
+                model.addAttribute("oximetro",oximetro);
+                System.out.println(oximetro.getActivo());
+                return "editar";
+            }
+            else{
+                attributes.addFlashAttribute("mensaje_error", "No hay pulsioximetro con el id seleccionado");
+                return "redirect:/netpulse/oximetros";
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("mensaje_error", "El id debe ser un numero entero");
             return "redirect:/netpulse/oximetros";
         }
     }
